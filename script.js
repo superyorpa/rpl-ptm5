4a16be0f0cbaaca50fa390923f832f7543efe153
Radit <primaalfiansyah@gmail.com>
2026-04-08 20:42:41 +0700

    feat: init project

---
+++ script.js
@@ -0,0 +1,32 @@
+function setMood(mood) {
+  const name = document.getElementById("nameInput").value;
+  const greeting = document.getElementById("greeting");
+
+  if (name.trim() === "") {
+    greeting.textContent = "Please enter your name first!";
+    return;
+  }
+
+  let message = "";
+  let bg = "";
+
+  if (mood === "happy") {
+    message = "Keep smiling and stay positive! 😊";
+    bg = "#f1c40f";
+  } 
+  else if (mood === "neutral") {
+    message = "Take it easy and go step by step 😐";
+    bg = "#7f8c8d";
+  } 
+  else if (mood === "sad") {
+    message = "Stay strong! You can do it 💙";
+    bg = "#3498db";
+  } 
+  else if (mood === "cool") {
+    message = "You're awesome 😎";
+    bg = "#2ecc71";
+  }
+
+  greeting.textContent = `Hello ${name}, ${message}`;
+  document.body.style.background = bg;
+}
